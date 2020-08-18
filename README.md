153aa8c92c56a67ace21fa151d6a0becb96f8d95
Chris Okerberg <seminative@gmail.com>
2020-08-17 23:36:13 -0400

    Added xml fetch

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 # Hack to detect hurricane
 
 The purpose of this repo is to demonstrate how to detect a hurricane using an rss feed. Mostly, this was written late at night for the lolz.
+
+### Libraries used
+- pip
+- chalice
+- requests
--- detect_hurricane/app.py
+++ detect_hurricane/app.py
@@ -1,10 +1,58 @@
 from chalice import Chalice
+import csv
+import requests
+import xml.etree.ElementTree as ET
+
+
+def loadRSS():
+    # url of rss feed
+    url = 'https://www.nhc.noaa.gov/rss_examples/index-at-20130605.xml'
+
+    # creating HTTP response object from given url
+    resp = requests.get(url)
+
+    # saving the xml file
+    with open('topnewsfeed.xml', 'wb') as f:
+        f.write(resp.content)
 
 app = Chalice(app_name='detect_hurricane')
 
 
+def parseXML(xmlfile):
+    # create element tree object
+    tree = ET.parse(xmlfile)
+
+    # get root element
+    root = tree.getroot()
+
+    # create empty list for news items
+    newsitems = []
+
+    # iterate news items
+    for item in root.findall('./channel/item'):
+
+        # empty news dictionary
+        news = {}
+
+        # iterate child elements of item
+        for child in item:
+
+            # special checking for namespace object content:media
+            if child.tag == '{http://search.yahoo.com/mrss/}content':
+                news['media'] = child.attrib['url']
+            else:
+                news[child.tag] = child.text.encode('utf8')
+
+                # append news dictionary to news items list
+        newsitems.append(news)
+
+        # return news items list
+    return newsitems
+
+
 @app.route('/')
 def index():
+    loadRSS()
     return {'hello': 'world'}
 
 
